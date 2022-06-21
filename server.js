fd2aac620bbfccffd6d03cdbdff6e5ed089c8da6
Adelaide <adelaide.craig@outlook.com>
2022-06-21 09:03:40 -0500

    config

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const app = express ()
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`PORT ${PORT}`))
+app.listen(PORT, () => console.log(`Shortening URL's on port ${PORT}🍒`))
