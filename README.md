4318b0986322977adf2f21fa731b07de70839194
Adelaide <adelaide.craig@outlook.com>
2022-06-21 11:07:17 -0500

    🍒

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Tiny-URL
 When posting on social media, character limits can be a real pain. Why not save precious characters with a URL shortener?
 
+# Getting Started 
+To check out this url shortener service:
+- fork and clone 
+- npm install
+- npm run dev
 
 
 
--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const express = require("express");
 
 const app = express();
 
+/// database
+const connection = require("./config/db.config");
+createConnection.once("open", () => console.log("DB connected"));
+// connection.on("error", () => console.log("Error"));
+
+// routes
+app.use(
+  express.json({
+    extended: false,
+  })
+);
+app.use("/", require("./routes/redirect"));
+app.use("/api/url", require("./routes/url"));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Shortening URL's on port ${PORT}🍒`));
